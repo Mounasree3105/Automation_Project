651889c386dae30ecdbe247fd5b1f220567bbfbf
Mounasree3105 <mounasree3105@gmail.com>
2023-03-01 17:35:18 +0000

    This is version 2

--- automation.sh
+++ automation.sh
@@ -11,3 +11,28 @@ name="Mounasree"
 s3_bucket="upgrad-mounasree"
 tar -cvf /tmp/$name-httpd-logs-$timestamp.tar /var/log/apache2/*.log
 aws s3 cp /tmp/$name-httpd-logs-$timestamp.tar s3://$s3_bucket/$name-httpd-logs-$timestamp.tar
+
+
+
+file_path="/tmp/$name-httpd-logs-$timestamp.tar"
+file_size=$(stat -c%s "$file_path")
+
+
+if [ -e /var/www/html/inventory.html ]; then
+    echo -e "httpd-logs  ${timestamp}  tar  ${file_size}" >> /var/www/html/inventory.html
+else
+    echo "Log Type      Date Created   Type  Size" > /var/www/html/inventory.html
+fi
+
+
+if [ -e /etc/cron.d/automation ]; then
+ echo "Cron job Running"
+else
+
+
+    
+
+    echo "0 0 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+
+    echo "Cron job started"
+fi
